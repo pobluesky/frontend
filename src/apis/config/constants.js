9bc8b66b2433dd82d7567dbadf5d39ca442135ec
Ubuntu <ubuntu@ip-172-31-38-219.ap-northeast-2.compute.internal>
2024-09-20 02:21:50 +0000

    pbs-288--feat: front change baseurl

--- src/apis/config/constants.js
+++ src/apis/config/constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://15.165.29.164:8000/api' : 'https://배포주소/api';
+const BASE_URL = 'http://15.165.29.164:8000/api';
 
 export default BASE_URL;
